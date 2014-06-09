26b446e44bf3cf215109bc11a8804fb36c0a9f64
Glenn Searby <glenn.searby@gmail.com>
2014-06-09 12:20:09 +0100

    Display some fake process nodes with an incorrectly counted interval probability and a name.

--- process-model.js
+++ process-model.js
@@ -1,18 +1,88 @@
 "use strict";
 
-/*global d3 */
+/*global d3, ProcessModel */
 
 var svg = d3.select("svg#model");
 
 var g = svg.append("g");
 
-var fakeData = g.selectAll("circle")
-	.data([1, 2, 3])
-	.enter()
-	.append("circle")
-	.attr("cx", 100)
-	.attr("cy", 100)
-	.attr("r", 50);
+var fakeData = [
+    ProcessModel.Node()
+	.name("process-modelling")
+	.description("The root node of our model, as a process.")
+	.localEvidence([0.25, 0.75]),
+    ProcessModel.Node()
+	.name("collecting")
+	.description("Another node of our model.")
+	.localEvidence([0.7, 0.9])
+];
+
+fakeData[0].x = 50;
+fakeData[0].y = 50;
+
+fakeData[1].x = 200;
+fakeData[1].y = 200;
+
+var nodes = g.selectAll("g")
+	.data(fakeData);
+
+nodes.exit().remove();
+
+var newNodes = nodes.enter()
+	.append("g")
+	.classed("process-node", true);
+
+newNodes
+    .append("rect")
+    .attr("width", "150px")
+    .attr("height", "50px");
+
+newNodes
+    .append("text")
+    .attr("y", "20px")
+    .attr("x", "75px")
+    .attr("textLength", "130px")
+    .attr("lengthAdjust", "spacingAndGlyphs")
+    .attr("text-anchor", "middle");
+
+nodes.attr("transform", function(d, i){
+    return "translate(" + d.x + "," + d.y + ")";
+});
+
+nodes.select("text")
+    .html(function(d, i){
+	return d.name();
+    });
+
+newNodes.append("g")
+    .classed("interval", "true")
+    .attr("transform", "translate(0,30)");
+
+var parts = nodes.selectAll(".interval")
+	.selectAll("rect")
+    	.data(function(d, i){
+	    var p = d.p();
+	    return [
+		{type: "success", width: p[0], x: 0},
+		{type: "uncertainty", width: p[1] - p[0], x: p[0]},
+		{type: "failure", width: 1 - p[1], x: p[1]}
+	    ];
+	});
+
+parts.enter()
+    .append("rect")
+    .attr("height", "15px");
+
+parts
+    .attr("class", function(d, i){
+	return d.type;
+    })
+    .attr("x", function(d, i){
+	return (15 + (120 * d.x)) + "px";
+    })
+    .attr("width", function(d, i){
+	return (120 * d.width) + "px";
+    });
 
 var zoom = d3.behavior.zoom()
 	.scaleExtent([0.1, 10])
@@ -21,3 +91,4 @@ var zoom = d3.behavior.zoom()
 	});
 
 zoom(svg);
+
--- process-node.js
+++ process-node.js
@@ -3,48 +3,111 @@
 /*global d3, ProcessModel */
 
 if (!ProcessModel) {
-    var OpenDataMap = {};
+    var ProcessModel = {};
 }
 
 ProcessModel.Node = function() {
-    var localW = 0.5;
-    var localE = [0, 1];
-    var edges = [];
+    var localN = 1,
+	localS = 1,
+	localE = [0, 1],
+	edges = [],
+	name = "",
+	description = "";
 
-    var combine = function(p, weight, evidence) {
-	throw "Not implemented";
+    var combine = function(p, necessity, sufficiency, evidence) {
+	/* TODO */
+	return evidence;
     };
 
-    return {
+    var node = {
 	localEvidence: function(evidence) {
 	    if (evidence) {
 		localE = evidence;
+		return node;
 	    }
 	    return localE;
 	},
-	localWeight: function(weight) {
-	    if (weight) {
-		localW = weight;
+	localNecessity: function(necessity) {
+	    if (necessity) {
+		localN = necessity;
+		return node;
 	    }
-	    return localW;
+	    return localN;
+	},
+	localSufficiency: function(sufficiency) {
+	    if (sufficiency) {
+		localS = sufficiency;
+		return node;
+	    }
+	    return localS;
 	},
 	edges: function() {
 	    return edges;
 	},
 	addEdge: function(edge) {
 	    edges.push(edge);
+	    return node;
 	},
 	p: function() {
+	    /* Our prior is total uncertainty. */
 	    var evidence = [0.0, 1.0];
 
-	    evidence = combine(evidence, localW, localE);
-
+	    /* Necessity and sufficiency must add up to 1. To enforce this, we first calculate the normalisation factor. */
+	    var nNorm = localN;
+	    var sNorm = localS;
 	    edges.forEach(function(edge){
-		evidence = combine(evidence, edge.weight(), edge.node().p();
+		nNorm += edge.necessity();
+		sNorm += edge.sufficiency();
+	    });
+
+	    /* We add the local evidence. */
+	    evidence = combine(evidence, localN/nNorm, localS/sNorm, localE);
+
+	    /* Then the evidence for each node it depends on. */
+	    edges.forEach(function(edge){
+		evidence = combine(evidence, edge.necessity()/nNorm, edge.sufficiency()/sNorm, edge.node().p());
 	    });
 	    	    
 	    return evidence;
+	},
+	name: function(n) {
+	    if (n) {
+		name = n;
+		return node;
+	    }
+	    return name;
+	},
+	description: function(d) {
+	    if (d) {
+		description = d;
+		return node;
+	    }
+	    return description;
 	}
-
     };
+    return node;
 };
+
+ProcessModel.Edge = function(node) {
+    var necessity = 1,
+	sufficiency = 1;
+
+    var edge = {
+	necessity : function(n) {
+	    if (n) {
+		necessity = n;
+		return edge;
+	    }
+	    return necessity;
+	},
+	sufficiency : function(s) {
+	    if (s) {
+		sufficiency = s;
+		return edge;
+	    }
+	    return sufficiency;
+	}
+    };
+    return edge;
+};
+
