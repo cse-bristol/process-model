a7a31a10a21ca09642547aff67316619589bcaee
Glenn Searby <glenn.searby@gmail.com>
2014-08-01 15:11:38 +0100

    Fixed modifying dependence and necessity/sufficiency being too slow in Firefox because of different scroll wheel units.

--- helpers.js
+++ helpers.js
@@ -7,13 +7,21 @@ if (!ProcessModel) {
 }
 
 ProcessModel.Util = function(){
+    var units = [
+	/* These are made up numbers, because I only need it to be close enough and do not know where to get real numbers from. */
+
+	1, // pixel
+	20, // pixels per line
+	20 * 20 // pixels per line * lines per page
+    ];
+
     return {
 	onScroll : function(selection, f) {
-	    selection.on("wheel.zoom", function(d, i){
+	    selection.on("wheel", function(d, i){
 		d3.event.stopPropagation();
 		d3.event.preventDefault();
-
-		var change = d3.event.deltaY * -0.0003;
+		
+		var change = d3.event.deltaY * units[d3.event.deltaMode] * -0.0003;
 
 		f(d, i, change);
 	    });
