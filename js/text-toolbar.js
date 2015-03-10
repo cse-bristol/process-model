dde377b882e4ad5fef1bbf8a4adf02841ab770ea
Glenn Searby <glenn.searby@gmail.com>
2015-03-10 16:41:22 +0000

    Clicking the 'submit' button makes a link.

--- js/text-toolbar.js
+++ js/text-toolbar.js
@@ -3,7 +3,15 @@
 /*global require, module*/
 
 var d3 = require("d3"),
-    svgEditableText = require("./svg-editable-text.js");
+    svgEditableText = require("./svg-editable-text.js"),
+    /*
+     Our blur event must be on a field inside the form, but hides the whole form.
+
+     However, blur might have fired because we clicked on something else inside the form, so give that a chance to happen before we remove the whole thing.
+
+     Yuck.
+     */
+    clickDelay = 100;
 
 var command = function(c) {
     return function() {
@@ -16,39 +24,44 @@ var command = function(c) {
 module.exports = function(dialogueContainer, toolbarContainer, transitions) {
     var target,
 	charWidth = 15,
-	dialogueThenCommand = function(fieldName, c) {
-	    var selectionRange;
 
-	    var form = dialogueContainer.append("form")
+	dialogueThenCommand = function(fieldName, c) {
+	    var selectionRange,
+		form = dialogueContainer.append("form")
 		    .classed("dialogue", true)
 		    .style("visibility", "hidden")
-	    .on("submit", function() {
-		d3.event.preventDefault();
-		d3.event.stopPropagation();
+		    .on("submit", function() {
+			d3.event.preventDefault();
+			d3.event.stopPropagation();
 
-		form.style("visibility", "hidden");
-		
-		if (target) {
-		    target.focus();
-		    document.getSelection().removeAllRanges();
-		    document.getSelection().addRange(selectionRange);
-		    if (!document.execCommand(c, false, field[0][0].value)) {
-			throw new Error("Could not exec browser command " + c + " with " + field.text());
-		    }
+			form.style("visibility", "hidden");
+			
+			if (target) {
+			    target.focus();
+			    document.getSelection().removeAllRanges();
+			    document.getSelection().addRange(selectionRange);
+			    if (!document.execCommand(c, false, field[0][0].value)) {
+				throw new Error("Could not exec browser command " + c + " with " + field.text());
+			    }
 
-		    d3.select(target)
-			.selectAll("a")
-			.style("contenteditable", false);
-		}
-	    });
+			    d3.select(target)
+				.selectAll("a")
+				.attr("contenteditable", false);			    
+			}
+		    }),
 
-	    var field = form
+		field = form
 		    .append("input")
 		    .attr("type", "text")
 		    .on("blur", function(d, i) {
-			form.style("visibility", "hidden");
+			window.setTimeout(
+			    function() {
+				form.style("visibility", "hidden");				
+			    },
+			    clickDelay
+			);
 		    });
-
+	    
 	    form.append("input")
 		.attr("type", "submit");
 
@@ -72,16 +85,16 @@ module.exports = function(dialogueContainer, toolbarContainer, transitions) {
 	},
 
 	commands = [
-	{icon: "B", styles: {"font-weight": "bold"}, f: command("bold")},
-	{icon: "I", styles: {"font-style": "italic"}, f: command("italic")},
-	{icon: "U", styles: {"text-decoration": "underline"}, f: command("underline")},
-	{icon: "•", f: command("insertUnorderedList")},
-	{icon: "1.", f: command("insertOrderedList")},
-	{icon: "⇐", f: command("outdent")},
-	{icon: "⇒", f: command("indent")},
-	{icon: "link", command: "createLink", f: dialogueThenCommand("Link-URL", "createLink")}, 
-	{icon: "img", command: "insertImage", f: dialogueThenCommand("Image-URL", "insertImage")} 
-    ];
+	    {icon: "B", styles: {"font-weight": "bold"}, f: command("bold")},
+	    {icon: "I", styles: {"font-style": "italic"}, f: command("italic")},
+	    {icon: "U", styles: {"text-decoration": "underline"}, f: command("underline")},
+	    {icon: "•", f: command("insertUnorderedList")},
+	    {icon: "1.", f: command("insertOrderedList")},
+	    {icon: "⇐", f: command("outdent")},
+	    {icon: "⇒", f: command("indent")},
+	    {icon: "link", command: "createLink", f: dialogueThenCommand("Link-URL", "createLink")}, 
+	    {icon: "img", command: "insertImage", f: dialogueThenCommand("Image-URL", "insertImage")} 
+	];
 
     var bar = toolbarContainer.append("g")
 	    .attr("id", "text-toolbar")
@@ -137,7 +150,7 @@ module.exports = function(dialogueContainer, toolbarContainer, transitions) {
 
     return {
 	blur: function(d, i) {
-	    bar.attr("visibility", "hidden");
+ 	    bar.attr("visibility", "hidden");
 	},
 	focus: function(d, i) {
 	    var rect = this.getBoundingClientRect();
