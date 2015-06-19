195ffeca89648c15537298c05921adacfbaf8237
Glenn Searby <glenn.searby@gmail.com>
2015-06-19 11:08:28 +0100

    Phantom edge when connecting nodes which indicates what you're connecting to is scaled and translated correctly again.

--- js/draw/draw.js
+++ js/draw/draw.js
@@ -64,7 +64,7 @@ module.exports = function(body, svg, queryString, getNodeCollection, getLayoutSt
 	    transitions, drawEdges.drawEdges, redrawNode, selectNodes,
 	    update
 	),
-	types = drawNodeTypesFactory(svg, redrawNode, transitions, viewport, getNodeCollection, getLayoutState, update),
+	types = drawNodeTypesFactory(g, redrawNode, transitions, viewport, getNodeCollection, getLayoutState, update),
 
 	drawNodeMargin = drawMarginsFactory(getNodeCollection, getLayoutState, viewport, transitions, update),
 	emphasis = emphasisFactory(defs),
