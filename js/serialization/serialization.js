c9ad719640994ac9ff15b6a8a8fb41a64c286622
Glenn Searby <glenn.searby@gmail.com>
2015-09-10 15:54:38 +0100

    Fixed perimeta import.

--- js/serialization/serialization.js
+++ js/serialization/serialization.js
@@ -19,7 +19,7 @@ module.exports = function(body, model) {
 		);
 	    },
 	    xml: function(fileName, content) {
-		model.fromNodes(
+		model.setFromNodes(
 		    perimetaDeserialize(content)
 		);
 	    }
